412a41c36179371c6316e13fa49f261476c8613a
Camila-Cidreira <107806886+Camila-Cidreira@users.noreply.github.com>
2023-10-31 16:21:47 -0300

    4 commit listar_usuario.php

--- listar_usuario.php
+++ listar_usuario.php
@@ -1,22 +1,34 @@
-<h1>Listar Usuários</h1>
-<?php 
-    include("config.php");
-    $sql= "SELECT * FROM usuarios";
-    $res=$conn->query($sql);
-    $total=$res->num_rows;
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Listar Usuários</title>
+    </head>
+    <body>
+        <h1>Listar Usuários</h1>
+        <?php 
+            include("config.php");
+            $sql= "SELECT * FROM usuarios";
+            $res=$conn->query($sql);
+            $total=$res->num_rows;
 
-    if($total>0){
-        while($row = $res->fetch_object()){
-?>         
-        <div>
-            <span><?php echo $row->id; ?></span>
-            <span><?php echo $row->nome; ?></span>
-            <span><?php echo $row->email; ?></span>
-            <span><?php echo $row->senha; ?></span>
-        </div>
-<?php }
-    }else{
-        print "<p>vazio</p>";
-    }
-?>
-<a href="index.php">Voltar</a>
+            if($total>0){
+                while($row = $res->fetch_object()){
+        ?>         
+                <div>
+                    <span><?php echo $row->id; ?></span>
+                    <span><?php echo $row->nome; ?></span>
+                    <span><?php echo $row->email; ?></span>
+                    <span><?php echo $row->senha; ?></span>
+            
+                </div>
+        <?php }
+            }else{
+                print "<p>vazio</p>";
+            }
+        ?>
+        <a href="index.php">Voltar</a>
+        
+    </body>
+</html>
