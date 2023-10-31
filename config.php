a9fc457d9969dff28eaceb2ca75ddfa470d8f640
Camila-Cidreira <107806886+Camila-Cidreira@users.noreply.github.com>
2023-10-31 17:17:45 -0300

    6 commit config.php

--- config.php
+++ config.php
@@ -1,18 +1,4 @@
 <?php 
-<<<<<<< HEAD
-    // define('HOST','Localhost');
-    // define('USER', 'root');
-    // define('PASS','');
-    // define('BASE', 'cadastro');
-    $hostname = "127.0.0.1";
-    $usuario= "root";
-    $senha="";
-    $bancodedados="cadastro";
-    $port="3306";
-    
-
-    // $conn = new MySqli(HOST, USER, PASS, BASE);
-=======
 
     $hostname = "localhost";
     $usuario= "root";
@@ -20,7 +6,6 @@
     $bancodedados="cadastro";
     $port="3307";
     
->>>>>>> e860b608ae61104fb6e031b56f6eba0f7fed3b45
     $conn = new mysqli($host, $usuario, $senha, $bancodedados, $port);
 
     if($conn-> connect_errno){
