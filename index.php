c5cc8a297c666c9fadf06153c6fb8e950ca3515b
Camila-Cidreira <107806886+Camila-Cidreira@users.noreply.github.com>
2023-10-31 17:20:01 -0300

    6 commit index.php

--- index.php
+++ index.php
@@ -5,31 +5,21 @@
   <head>
      <body>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<<<<<<< HEAD
-                <a class="navbar-brand" href="novo_usuario.php">Cadastro</a>
-                <a class="navbar-brand" href="listar_usuario.php">Usuarios</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" 
-                aria-expanded="false" aria-label="Alterna navegação">
-                <span class="navbar-toggler-icon"></span>
-=======
+
                 <a class="navbar-brand" href="novo_usuario.php" target="_blank">Cadastro</a>
                 <a class="navbar-brand" href="listar_usuario.php" target="_blank">Usuarios</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" 
                 aria-expanded="false" aria-label="Alterna navegação">
                 <span class="navbar-toggler-icon"></span>
                 <title>Gerenciador de Livros></title>
->>>>>>> e860b608ae61104fb6e031b56f6eba0f7fed3b45
 
             <nav>
             <div class="container">
                 <div class="row"> 
                     <div class="col mt-5">
                         <?php
-<<<<<<< HEAD
-                            // include("config.php");   
-=======
+
                             include("config.php");   
->>>>>>> e860b608ae61104fb6e031b56f6eba0f7fed3b45
                         
                             switch(@$_REQUEST["page"]){
                                 case "Cadastro":
@@ -52,11 +42,7 @@
         </body>
     </head>
 </html>  
-<<<<<<< HEAD
-=======
 
-  
->>>>>>> e860b608ae61104fb6e031b56f6eba0f7fed3b45
 
 
 
