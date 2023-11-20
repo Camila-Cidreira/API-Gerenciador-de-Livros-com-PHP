10746fd34f80348a3f17f6b13fa8a85189d7f1af
Camila-Cidreira <107806886+Camila-Cidreira@users.noreply.github.com>
2023-11-20 19:26:11 -0300

    index.php - 7 commit

--- index.php
+++ index.php
@@ -3,23 +3,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
-     <body>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cadastro</title>
+    
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="novo_usuario.php" target="_blank">Cadastro</a>
+            <a class="navbar-brand" href="listar_usuario.php" target="_blank">Usuarios</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" 
+            aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+                
 
-                <a class="navbar-brand" href="novo_usuario.php" target="_blank">Cadastro</a>
-                <a class="navbar-brand" href="listar_usuario.php" target="_blank">Usuarios</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" 
-                aria-expanded="false" aria-label="Alterna navegação">
-                <span class="navbar-toggler-icon"></span>
-                <title>Gerenciador de Livros></title>
-
-            <nav>
-            <div class="container">
-                <div class="row"> 
-                    <div class="col mt-5">
-                        <?php
-
-                            include("config.php");   
+        <nav>
+        <div class="container">
+            <div class="row"> 
+                <div class="col mt-5">
+                    <?php
+                        include("config.php");   
                         
                             switch(@$_REQUEST["page"]){
                                 case "Cadastro":
@@ -33,15 +36,19 @@
                                     include("listar_usuario.php");
                                 break;
                                 default:
-                                    print "<h1>Bem-vindo ao seu Gerenciador de Livros!</h1>";
+                                    print "<h1>Bem-vindo a sua biblioteca online!</h1>";
                             }
                         ?>    
                     </div>  
                 </div>
+                
             </div>
-        </body>
-    </head>
+        </div>    
+
+    </body>
+    
 </html>  
+
 
 
 
