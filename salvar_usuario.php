6fa058183c52b0e17eb00cda5800453cf8fe4595
Camila-Cidreira <107806886+Camila-Cidreira@users.noreply.github.com>
2023-11-20 19:30:26 -0300

    salvar_usuario.php 7 commit

--- salvar_usuario.php
+++ salvar_usuario.php
@@ -1,29 +1,43 @@
-<?php 
-    include("config.php");
-    switch ($_REQUEST["acao"]) {
-        case 'cadastrar':
-            $nome = $_POST["nome"];
-            $email = $_POST["email"];
-            $senha = $_POST["senha"];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salvar Usuário</title>
+</head>
+<body>
+    <h1>Seu Usuário foi Salvo!</h1>
+    <button><a href="login.php">Voltar</a></button>
+    <?php 
+        include("config.php");
+        switch ($_REQUEST["acao"]) {
+            case 'cadastrar':
+                $nome = $_POST["nome"];
+                $email = $_POST["email"];
+                $senha = $_POST["senha"];
 
-            $sql = "INSERT INTO usuarios(nome, email, senha) VALUES('{$nome}','{$email}', '{$senha}')";
-            $res = $conn->query($sql);
+                $sql = "INSERT INTO usuarios(nome, email, senha) VALUES('{$nome}','{$email}', '{$senha}')";
+                $res = $conn->query($sql);
 
-        break;
-            if($res==true){
-                print "<script>alert('usuario cadastrado')</script>";
-                print "<script>location.href='listar_usuario.php'</script>";
-            }
             break;
+                if($res==true){
+                    print "<script>alert('usuario cadastrado')</script>";
+                    print "<script>location.href='listar_usuario.php'</script>";
+                }
+                break;
 
-        case 'editar':
-            //code...
-        break;
-        case 'excluir':
-            //code...
-        break;
-            
+            case 'editar':
+                //code...
+            break;
+            case 'excluir':
+                //code...
+            break;
+                
 
 
-    }
-?>
+        }
+    ?>
+    
+</body>
+</html>
+
