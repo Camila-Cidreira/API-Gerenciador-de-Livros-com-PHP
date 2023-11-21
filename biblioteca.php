a39e9ec51d3917f2c2698d33cfca67e72c496238
Camila-Cidreira <107806886+Camila-Cidreira@users.noreply.github.com>
2023-11-20 21:05:48 -0300

    biblioteca.php 8 commit

--- biblioteca.php
+++ biblioteca.php
@@ -1,97 +1,201 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>biblioteca</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.elo7.com.br%2Fpapel-de-parede-adesivo-livros-decoracao-biblioteca%2Fdp%2FB532F4&psig=AOvVaw1OGGjzESVRh7q9TELt_TNY&ust=1700324601474000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMCoq_C4y4IDFQAAAAAdAAAAABAE') center/cover no-repeat; /* Substitua pelo caminho real da sua imagem */
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+
+        #biblioteca {
+            width: 80%;
+            margin: 20px auto;
+            padding: 20px;
+            background: linear-gradient(to bottom, #ffffff, #f2f2f2);
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 12px;
+            transition: transform 0.3s ease-in-out;
+        }
+
+        #biblioteca:hover {
+            transform: scale(1.01);
+        }
+
+        h1 {
+            color: #008080;
+            text-align: center;
+        }
+
+        article {
+            margin-bottom: 30px;
+            background: #fff;
+            border-radius: 12px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+            transition: transform 0.3s ease-in-out;
+        }
+
+        article:hover {
+            transform: scale(1.01);
+        }
+
+        h2 {
+            color: #008080;
+            margin-bottom: 10px;
+        }
+
+        img {
+            width: 100%;
+            max-width: 15%; /* Reduzi o valor aqui */
+            height: auto;
+            border-radius: 12px 12px 0 0;
+            display: block;
+            transition: transform 0.3s ease-in-out;
+        }
+
+        img:hover {
+            transform: scale(1.1);
+        }
+
+        h3 {
+            color: #008080;
+            margin-top: 10px;
+        }
+
+        p {
+            line-height: 1.6;
+            padding: 15px;
+            margin: 0;
+        }
+
+        a {
+            color: #008080;
+            text-decoration: none;
+            font-weight: bold;
+            transition: color 0.3s ease-in-out;
+        }
+
+        a:hover {
+            text-decoration: underline;
+            color: #006666;
+        }
+
+        /* Responsividade */
+        @media screen and (max-width: 768px) {
+            #biblioteca {
+                width: 90%;
+            }
+        }
+
+        footer {
+            text-align: center;
+            margin-top: 20px;
+            padding: 10px;
+            border-top: 1px solid #ddd;
+            background: #fff;
+            border-radius: 0 0 12px 12px;
+            box-shadow: 0 -4px 8px rgba(0, 0, 0, 0.1);
+        }
+
+        footer a {
+            color: #008080;
+            text-decoration: none;
+            font-weight: bold;
+            transition: color 0.3s ease-in-out;
+        }
+
+        footer a:hover {
+            text-decoration: underline;
+            color: #006666;
+        }
+    </style>
 </head>
 <body>
     <div id="biblioteca">
 
-    <h1>Biblioteca</h1>
-    
-          <div class="container">
-        <h2>A maldição do mar</h2>
-        <img src="https://files.passeidireto.com/934e941b-426f-408b-8f2f-ea41518a4e53/bg1.png" alt="" width="200" height="300" >
-        <h3>Descrição</h3>
-        <p>A maodição do mar é mais que uma história de terror </p>
-        <p>é uma fábula com substâncias e profundidade</p>
-        <p>sobre magia e maldições </p>
-        <p>traição e vingança</p>
-        <p>pórem o mais importante</p>
-        <p>uma hitória sobre a qualidade redentora do amor </p>
-          </div>
-            <div>
-            <h2>O bosque das coisas perdidas </h2>
-               <p> <img src="https://m.media-amazon.com/images/I/81Jt60mmEzL._AC_UF1000,1000_QL80_.jpg" alt="" width="200" height="300" > </p>
-               <h3>Descrição</h3>
-               <p>Quando dorme,a floresta lhe</p>
-               <p>permitira uma passagem segura</p>
-               <p>mas se despertar esteja preparado</p>
-               <p>para correr </p>
-            </div>
-                    <div>
-                        <h2>Harry potter </h2>
-                        <p><img src="https://m.media-amazon.com/images/I/81ibfYk4qmL._AC_UF1000,1000_QL80_.jpg" alt="" width="200" height="300"> </p>
-                        <h3>Descrição</h3>
-                        <p>Harry potter nunca tinha ouvido </p>
-                        <p>falar em hogwarts até o momento</p>
-                        <p>em que cartas começaram a aparecer </p>
-                        <p>no capacho do número 4 na rua dos </p>
-                        <p>alfeneiros.</p>
-                        <p>Endereçadas com tinta verde </p>
-                        <p>em pergaminho amarelado com um </p>
-                        <p>lacre púrpura, elas são rapidamente</p>
-                        <p>confiscadas por seus tios terríveis</p>
-                        <p>e então, no décimo primeiro aniversário</p>
-                        <p>de harry um homem gigantesco com olhos </p>
-                        <p>luzindo como besouros negros chamado </p>
-                        <p>Rúbeo Hagrid entra intempestivamente</p>
-                        <p>com uma notícia assombrosa </p>
-                        <p>harry potter é um bruxo e tem uma </p>
-                        <p>vaga na escola de magia e bruxaria de hogwarts.</p>
-                        <p>Uma aventura inacreditável está para começar!</p>
-                                <div>
-                                    <h2>Star dust</h2>
-                                    <p><img src="https://m.media-amazon.com/images/I/71FTJMMaOvS._AC_UF1000,1000_QL80_.jpg" alt="" width="200" height="300"> </p>
-                                    <h3>Descrição</h3>
-                                    <p>-Bem,você quer voltar,não quer? Lá pra cima</p>
-                                    <p>para o céu.Brilhar de novo à noite.</p>
-                                    <p>Ela olhou para ele e fez que não.</p>
-                                    <p>-Isso não acontece-explicou. - as estrelas </p>
-                                    <p>caem. Elas não voltam para seu lugar.</p>
-                                    <p>-Você poderia ser a primeira - disse ele. </p>
-                                    <p>-Só precisa acreditar.</p>
-                                </div>
-                                            <div>
-                                                <h2> A garota do lago</h2>
-                                                <p><img src="https://m.media-amazon.com/images/I/81LRk6+p1HL._AC_UF1000,1000_QL80_.jpg" alt="" width="200" height="300"> </p>
-                                                <h3>Descrição</h3>
-                                                <p>Alguns lugares parecem belos demais para </p>
-                                                <p>serem tocados pelo horror </p>
-                                            </div>
-                                                    <div>
-                                                        <h2> A rainha vermelha</h2>
-                                                        <p><img src="https://m.media-amazon.com/images/I/41pZtBIcGhL.jpg" alt="" width="200" height="300"> </p>
-                                                        <h3>Descrição</h3>
-                                                        <p>O mundo de mare barrow é dividido pelo sangue:</p>
-                                                        <p>vermelho ou prateado</p>
-                                                        <p>mare e sua família são vermelhos:</p>
-                                                        <p>plebeus, humildes, destinados a servir</p>
-                                                        <p>uma elite prateada cujo os poderes os</p>
-                                                        <p>tornam quase deuses </p>
-                                                    </div>
-                                                                <div>
-                                                                    <h2>A menina que roubava livros </h2>
-                                                                    <p><img src="https://m.media-amazon.com/images/I/41L1YWkhlkL.jpg" alt="" width="200" height="300"></p>
-                                                                    <h3>Descrição</h3>
-                                                                    <p>Quando a morte lhe conta uma história</p>
-                                                                    <p>você deve parar para ler </p>
-                                                                </div>
-                
-                        
-                    </div>
-        
-    
+        <h1>Biblioteca</h1>
+
+        <article>
+            <h2>A maldição do mar</h2>
+            <img src="https://files.passeidireto.com/934e941b-426f-408b-8f2f-ea41518a4e53/bg1.png" alt="A maldição do mar" width="100%" height="300">
+            <h3>Descrição</h3>
+            <p>A maldição do mar é mais do que uma história de terror.</p>
+            <p>É uma fábula com substância e profundidade,</p>
+            <p>sobre magia e maldições, traição e vingança,</p>
+            <p>porém o mais importante, uma história sobre a qualidade redentora do amor.</p>
+            <p>Comentários: Excelente livro, altamente recomendado!</p>
+        </article>
+
+        <article>
+            <h2>O bosque das coisas perdidas</h2>
+            <img src="https://m.media-amazon.com/images/I/81Jt60mmEzL._AC_UF1000,1000_QL80_.jpg" alt="O bosque das coisas perdidas" width="100%" height="300">
+            <h3>Descrição</h3>
+            <p>Quando dorme, a floresta lhe permitirá uma passagem segura,</p>
+            <p>mas se despertar esteja preparado para correr.</p>
+            <p>Comentários: Uma aventura mágica e intrigante.</p>
+        </article>
+
+        <article>
+            <h2>Harry Potter</h2>
+            <img src="https://m.media-amazon.com/images/I/81ibfYk4qmL._AC_UF1000,1000_QL80_.jpg" alt="Harry Potter" width="100%" height="300">
+            <h3>Descrição</h3>
+            <p>Harry Potter nunca tinha ouvido falar em Hogwarts até o momento</p>
+            <p>em que cartas começaram a aparecer no capacho do número 4 na rua dos alfeneiros.</p>
+            <p>Endereçadas com tinta verde em pergaminho amarelado com um lacre púrpura,</p>
+            <p>elas são rapidamente confiscadas por seus tios terríveis e então, no décimo primeiro aniversário</p>
+            <p>de Harry, um homem gigantesco com olhos luzindo como besouros negros chamado</p>
+            <p>Rúbeo Hagrid entra intempestivamente com uma notícia assombrosa:</p>
+            <p>Harry Potter é um bruxo e tem uma vaga na escola de magia e bruxaria de Hogwarts.</p>
+            <p>Uma aventura inacreditável está para começar!</p>
+            <p>Comentários: Um clássico da literatura fantástica.</p>
+        </article>
+
+        <article>
+            <h2>Star Dust</h2>
+            <img src="https://m.media-amazon.com/images/I/71FTJMMaOvS._AC_UF1000,1000_QL80_.jpg" alt="Star Dust" width="100%" height="300">
+            <h3>Descrição</h3>
+            <p>"Bem, você quer voltar, não quer? Lá pra cima para o céu.</p>
+            <p>Brilhar de novo à noite." Ela olhou para ele e fez que não.</p>
+            <p>"Isso não acontece," explicou. "As estrelas caem. Elas não voltam para seu lugar."</p>
+            <p>"Você poderia ser a primeira," disse ele. "Só precisa acreditar."</p>
+            <p>Comentários: Uma história encantadora sobre sonhos e perseverança.</p>
+        </article>
+
+        <article>
+            <h2>A garota do lago</h2>
+            <img src="https://m.media-amazon.com/images/I/81LRk6+p1HL._AC_UF1000,1000_QL80_.jpg" alt="A garota do lago" width="100%" height="300">
+            <h3>Descrição</h3>
+            <p>Alguns lugares parecem belos demais para serem tocados pelo horror.</p>
+            <p>Comentários: Um thriller misterioso que prende a atenção.</p>
+        </article>
+
+        <article>
+            <h2>A rainha vermelha</h2>
+            <img src="https://m.media-amazon.com/images/I/41pZtBIcGhL.jpg" alt="A rainha vermelha" width="100%" height="300">
+            <h3>Descrição</h3>
+            <p>O mundo de Mare Barrow é dividido pelo sangue:</p>
+            <p>vermelho ou prateado.</p>
+            <p>Mare e sua família são vermelhos: plebeus, humildes, destinados a servir</p>
+            <p>uma elite prateada cujo os poderes os tornam quase deuses.</p>
+            <p>Comentários: Uma distopia envolvente com reviravoltas surpreendentes.</p>
+        </article>
+
+        <article>
+            <h2>A menina que roubava livros</h2>
+            <img src="https://m.media-amazon.com/images/I/41L1YWkhlkL.jpg" alt="A menina que roubava livros" width="100%" height="300">
+            <h3>Descrição</h3>
+            <p>Quando a morte lhe conta uma história, você deve parar para ler.</p>
+            <p>Comentários: Uma narrativa emocionante e comovente.</p>
+        </article>
+
+    </div>
+
+    <footer>
+        <p><a href="login.php">Voltar à página inicial</a></p>
+        <p>&copy; 2023 Biblioteca Virtual</p>
+    </footer>
 </body>
-</html
+</html>
